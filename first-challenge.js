8a8fcfcef30a2f0bdfe86ea644c43c2b438684f2
Hello <31024588+DSep@users.noreply.github.com>
2018-04-02 01:11:21 -0700

    Success.

--- first-challenge.js
+++ first-challenge.js
@@ -1,50 +1,64 @@
+// const GRID_10 = [
+//     ["", "", "", "^", "", "", "", "", "", ""],
+//     ["", "", "", "", "~", "", "", "", "", ""],
+//     ["", "", "", "", "^", "^", "", "", "", ""],
+//     ["", "", "", "", "^", "^", "", "", "", ""],
+//     ["", "", "", "", "", "", "", "", "", ""],
+//     ["~", "", "", "", "", "", "", "", "", ""],
+//     ["", "", "", "", "", "", "", "", "", ""],
+//     ["", "^", "~", "~", "", "", "", "^", "", ""],
+//     ["", "^", "", "~", "~", "", "", "", "", ""],
+//     ["", "^", "", "", "~", "~", "", "", "", ""],
+// ];
+
 const GRID = [
-    ["", "", "", "^", "", "", "", "", "", ""],
-    ["", "", "", "", "~", "", "", "", "", ""],
-    ["", "", "", "", "^", "^", "", "", "", ""],
-    ["", "", "", "", "^", "^", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "^", "~", "~", "", "", "", "^", "", ""],
-    ["", "^", "", "~", "~", "", "", "", "", ""],
-    ["", "^", "", "", "~", "~", "", "", "", ""],
+    ["", "", "", "^", "", "", "", "", ""],
+    ["", "", "", "", "~", "", "", "", ""],
+    ["", "", "", "", "^", "^", "", "", ""],
+    ["", "", "", "", "^", "^", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["~", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "^", "~", "~", "", "", "", "^", ""],
+    ["", "^", "", "~", "~", "", "", "", ""],
+    ["", "^", "", "", "~", "~", "", "", ""],
 ];
 
-function gridSize() {
+
+let gridSize = function gridSize(){
     let columns;
     columns = GRID[0].length;
-    if (columns === undefined){
-        console.log("Columns undefined!");
-        console.log(columns);
-    }
     let rows;
     let i = 0;
-    while (GRID[0][i] !== undefined) {
+    while (i < GRID.length) {
         i++;
     }
     rows = i;
-    // for (i = 0; i < array.length; i++) {
-    //   rows = i + 1;
-
-    // }
     let answer = columns + " x " + rows;
-
     return answer;
 }
 
-
-
-
-// if (rows === undefined){
-//   console.log(rows);
-//   console.log("Columns undefined!");
-
+// function gridSize() {
+//     let columns;
+//     columns = GRID[0].length;
+//     if (columns === undefined){
+//         console.log("Columns undefined!");
+//         console.log(columns);
+//     }
+//     let rows;
+//     let i = 0;
+//     while (GRID[0][i] !== undefined) {
+//         i++;
+//     }
+//     rows = i;
+//     // for (i = 0; i < array.length; i++) {
+//     //   rows = i + 1;
+//
+//     // }
+//     let answer = columns + " x " + rows;
+//
+//     return answer;
 // }
 
-//     if (answer === undefined) {
-//   console.log("Columns undefined!");
-//   console.log(answer);
-// }
 var answer = gridSize(GRID);
 console.log(answer);
