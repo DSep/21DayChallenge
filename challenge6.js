2095eab73ea38b6ef14d9e30f269bf0fc83a127d
Hello <31024588+DSep@users.noreply.github.com>
2018-04-07 22:49:11 -0700

    Finished 6th challenge.

--- challenge6.js
+++ challenge6.js
@@ -12,17 +12,21 @@ const GRID = [
 ];
 
 
-let lightRow = function lightRow(int){
+let lightRow = function lightRow(givenRow){
     let columns;
     columns = GRID[0].length;
     let rows;
     let i = 0;
     let cellContents = [];
-    while (i < GRID.length) {
-        cellContents = GRID[row][column] + "";
-        i++;
+    // while (i < GRID.length) {
+    //     i++;
+    // }
+    // rows = i;
+    let j = 0;
+    while(j < columns) {
+        cellContents.push(GRID[givenRow][j] + "");
+        j++;
     }
-    rows = i;
     return cellContents;
 }
 
