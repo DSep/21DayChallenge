d5a85bf46e8e021d9acf8020ed16d93b4a4b0442
Hello <31024588+DSep@users.noreply.github.com>
2018-04-10 00:30:14 -0700

    Finished challenge 9.

---
+++ challenge9.js
@@ -0,0 +1,28 @@
+const GRID = [
+    ["TEST", "", "", "^", "", "", "", "", "", ""],
+    ["", "", "", "", "~", "", "", "", "", ""],
+    ["", "", "", "", "^", "^", "", "", "", ""],
+    ["", "", "", "", "^", "^", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["~", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "^", "~", "~", "", "", "", "^", "", ""],
+    ["", "^", "", "~", "~", "", "", "", "", ""],
+    ["", "^", "", "", "~", "~", "", "", "", ""],
+];
+
+let isSafe = function isSafe(string)  {
+    let columns;
+    columns = GRID[0].length;
+    let i = GRID.length;
+    let rows;
+    let cellContents;
+    rows = i + 1 - 1;
+    let selecColumStr = string[0];
+    let selecColumInt = selecColumStr.charCodeAt(0) - 65;
+    let selecRow = Number(string[1]) - 1;
+    cellContents = GRID[selecRow][selecColumInt] + "";
+    return cellContents !== "~" && cellContents !== "^";
+}
+
+console.log(isSafe('D7'));
