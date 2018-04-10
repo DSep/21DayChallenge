93d72a36661d96a308ecea75f7593abb7ee18720
Hello <31024588+DSep@users.noreply.github.com>
2018-04-10 00:42:12 -0700

    Improvements on old challenges.

--- fourth-challenge.js
+++ fourth-challenge.js
@@ -29,3 +29,19 @@ let isRock = function lightCell(string){
 }
 
 console.log(isRock('D4'));
+
+// Improved:
+
+// let isRock = function isRock(string)  {
+//     let columns;
+//     columns = GRID[0].length;
+//     let i = GRID.length;
+//     let rows;
+//     let cellContents;
+//     rows = i + 1 - 1;
+//     let selecColumStr = string[0];
+//     let selecColumInt = selecColumStr.charCodeAt(0) - 65;
+//     let selecRow = Number(string[1]) - 1;
+//     cellContents = GRID[selecRow][selecColumInt] + "";
+//     return cellContents === "^";
+// }
