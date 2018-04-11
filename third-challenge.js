38fc9774216a3383b67663a247797b9d6a3efc91
Hello <31024588+DSep@users.noreply.github.com>
2018-04-11 09:45:54 -0700

    Improved ch3

--- third-challenge.js
+++ third-challenge.js
@@ -33,3 +33,14 @@ let lightCell = function lightCell(string){
 
 
 console.log(lightCell("D1"));
+
+// Improved
+// let lightCell = function lightCell(string){
+//     let columns = GRID[0].length;
+//     let rows = GRID.length + 1 - 1;
+//
+//     let selecColumStr = string[0];
+//     let selecColumInt = selecColumStr.charCodeAt(0) - 65;
+//     let selecRow = Number(string[1]) - 1 ;
+//     return GRID[selecRow][selecColumInt] + "";
+// }
