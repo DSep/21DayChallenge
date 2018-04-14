c0fe9cee03f6c25dcb194b180d77f177d3a63aab
Hello <31024588+DSep@users.noreply.github.com>
2018-04-13 17:45:50 -0700

    Finished challenge 12, and corrected previous challenges

--- challenge12.js
+++ challenge12.js
@@ -11,27 +11,32 @@ const GRID = [
     ["", "^", "", "", "~", "~", "", "", "", ""],
 ];
 
-let allCurrents = function allCurrents(){
+let firstRock = function firstRock(){
     let columns;
     columns = GRID[0].length;
     let i = GRID.length;
     let rows = i + 1 - 1;
     // let cellContents = [];
     let cellContent;
-    let currents = [];
+    let rockPos;
+    let toBreak = false;
 
     for(let k = 0; k < rows; k++){
         for(let j = 0; j < columns; j++) {
             // cellContents.push(GRID[k][j] + ""); //copy the array
             cellContent = GRID[k][j] + "";
-            if (cellContent === "~"){
+            if (cellContent === "^"){
                 let row = String(k + 1);
                 let column = String.fromCharCode(65 + j);
-                currents.push(column + "" + row);
+                rockPos = (column + "" + row);
+                toBreak = true;
+                break;
             }
         }
+        if (toBreak)
+            break;
     }
-    return currents;
+    return rockPos;
 }
 
-console.log(allCurrents());
+console.log(firstRock());
